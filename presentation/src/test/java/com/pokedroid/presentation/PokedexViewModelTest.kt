95542f73629c6d1df5e7d8417bf6b008c5d9205a
Ilke Kelesoglu <ilkekelesoglu@Ilke-Kelesoglu.local>
2018-09-12 16:05:16 +0200

    Add failure tests for PokedexViewModel

--- presentation/src/test/java/com/pokedroid/presentation/PokedexViewModelTest.kt
+++ presentation/src/test/java/com/pokedroid/presentation/PokedexViewModelTest.kt
@@ -14,10 +14,11 @@ import org.junit.jupiter.api.extension.ExtendWith
 @ExtendWith(InstantTaskExecutorExtension::class)
 class PokedexViewModelTest {
 
-    val retrieveLocations: RetrieveLocations = mock()
-    val retrievePokemons: RetrievePokemons = mock()
-
-    val tested = PokedexViewModel(retrievePokemons, retrieveLocations)
+    private val retrieveLocations: RetrieveLocations = mock()
+    private val retrievePokemons: RetrievePokemons = mock()
+    private val tested = PokedexViewModel(retrievePokemons, retrieveLocations)
+    private val liveDataValue = tested.pokedexLiveData.value
+    private val throwable = Throwable()
 
     @Test
     fun `correct data will be output - happy path`() {
@@ -31,12 +32,56 @@ class PokedexViewModelTest {
         tested.onBind()
 
         // assertion
-        val liveDataValue = tested.pokedexLiveData.value
         assertEquals(PokedexScreenState.Data(expectedPokemons.size, expectedLocations.size), liveDataValue)
     }
 
-    //@Test
-    // if retrieve pokemons fails, what happens
-    // if retrieve location fails, what happens
-    // if both fail
+
+    @Test
+    fun `error will be shown - retrieve pokemons fails`() {
+        // setting up the mocking
+        //Approach 1: Directly return error instead of valid expectedPokemons
+        val expectedLocations = listOf(Location(1, "asdas"))
+        whenever(retrieveLocations.retrieveBehaviorStream(Unit)).thenReturn(Observable.just(expectedLocations))
+        whenever(retrievePokemons.retrieveBehaviorStream(Unit)).thenReturn(Observable.error(throwable))
+
+        // test
+        tested.onBind()
+
+        // assertion
+        assertEquals((PokedexScreenState.Error("Ups!")), liveDataValue)
+
+    }
+
+    @Test
+    fun `error will be shown - retrieve locations fails`() {
+        // setting up the mocking
+        // Approach 2: Made expectedLocations null so that it would throw error during onBind() call
+        val expectedLocations = null
+        val expectedPokemons = listOf(Pokemon("1", "asdas"), Pokemon("1", "asdas"))
+        whenever(retrieveLocations.retrieveBehaviorStream(Unit)).thenReturn(Observable.just(expectedLocations))
+        whenever(retrievePokemons.retrieveBehaviorStream(Unit)).thenReturn(Observable.just(expectedPokemons))
+
+        // test
+        tested.onBind()
+
+        // assertion
+        assertEquals((PokedexScreenState.Error("Ups!")), liveDataValue)
+    }
+
+    @Test
+    fun `error will be shown - retrieve pokemons & locations  fail`() {
+        // setting up the mocking
+        val expectedLocations = null
+        val expectedPokemons = null
+        whenever(retrieveLocations.retrieveBehaviorStream(Unit)).thenReturn(Observable.just(expectedLocations))
+        whenever(retrievePokemons.retrieveBehaviorStream(Unit)).thenReturn(Observable.just(expectedPokemons))
+
+        // test
+        tested.onBind()
+
+        // assertion
+        // I'm a bit confused about the error messages here, not sure which one would be thrown for these cases
+        assertEquals((PokedexScreenState.Error("Yuasdjas")), liveDataValue)
+    }
+
 }
