1c7fbf817cc59cedede8912377e1d7e71bf82a84
fabiocarballo <fabio.carballo@n26.com>
2018-09-12 11:00:39 +0200

    NO-TICKET: add some testing dependencies for the presentation module

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,29 +1,35 @@
 object Versions {
-    val kotlin = "1.2.60"
-    val rxJava2 = "2.2.1"
-    val options = "0.10"
-    val junit4 = "4.12"
-    val junit5 = "5.1.0"
-    val mockitoKotlin = "2.0.0-RC1"
-    val lifecycleVersion = "2.0.0-rc01"
+    const val kotlin = "1.2.60"
+    const val rxJava2 = "2.2.1"
+    const val options = "0.10"
+    const val junit4 = "4.12"
+    const val junit5AndroidPlugin = "1.0.32"
+    const val junit5 = "5.1.0"
+    const val mockitoKotlin = "2.0.0-RC1"
+    const val lifecycleVersion = "2.0.0-rc01"
+    const val junitPlatform = "1.2.0"
+    const val junitJupiter = "5.2.0"
+    const val junitVintage = "5.2.0"
+    const val architectureComponents = "1.1.1"
 }
 
 object Dependencies {
-    val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
-    val rxJava2 = "io.reactivex.rxjava2:rxjava:${Versions.rxJava2}"
-    val options = "com.github.peter-tackage.kotlin-options:kotlin-options:${Versions.options}"
-    val rxJavaOptions = "com.github.peter-tackage.kotlin-options:kotlin-options-rxjava2-extensions:${Versions.options}"
-    val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycleVersion}"
-    val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycleVersion}"
+    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+    const val rxJava2 = "io.reactivex.rxjava2:rxjava:${Versions.rxJava2}"
+    const val options = "com.github.peter-tackage.kotlin-options:kotlin-options:${Versions.options}"
+    const val rxJavaOptions = "com.github.peter-tackage.kotlin-options:kotlin-options-rxjava2-extensions:${Versions.options}"
+    const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycleVersion}"
+    const val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycleVersion}"
 
     // Testing Dependencies
-    val junit4 = "junit:junit:${Versions.junit4}"
-    val jupiterApi = "org.junit.jupiter:junit-jupiter-api:${Versions.junit5}"
-    val jupiterEngine = "org.junit.jupiter:junit-jupiter-engine:${Versions.junit5}"
-    val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoKotlin}"
+    const val junit4 = "junit:junit:${Versions.junit4}"
+    const val jupiterApi = "org.junit.jupiter:junit-jupiter-api:${Versions.junit5}"
+    const val jupiterEngine = "org.junit.jupiter:junit-jupiter-engine:${Versions.junit5}"
+    const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoKotlin}"
+    const val architectureComponentsTesting = "android.arch.core:core-testing:${Versions.architectureComponents}"
 }
 
 object Modules {
-    val domain = ":domain"
-    val presentation = ":presentation"
+    const val domain = ":domain"
+    const val presentation = ":presentation"
 }
