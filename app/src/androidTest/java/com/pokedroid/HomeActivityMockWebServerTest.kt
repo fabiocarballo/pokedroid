dbcc7ac3eb3a5286cbc30b87c37570ec2ac4bd3c
Ilke Kelesoglu <ilkekelesoglu@ip-10-21-101-119.eu-central-1.compute.internal>
2018-11-02 15:41:17 +0100

    Fix test classes names

--- app/src/androidTest/java/com/pokedroid/HomeActivityMockWebServerTest.kt
+++ app/src/androidTest/java/com/pokedroid/HomeActivityMockWebServerTest.kt
@@ -19,7 +19,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class HomeActivityMockWebServer {
+class HomeActivityMockWebServerTest {
 
     @get:Rule
     var rule = ActivityTestRule(HomeActivity::class.java)
